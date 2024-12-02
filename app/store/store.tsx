1e411c0ef0351e8ae595c8677054e6e734d7577c
STF 5 <supermappin@gmail.com>
2024-12-02 09:43:38 +0900

    使用していない状態と管理メソッドをコメントアウト

--- app/store/store.tsx
+++ app/store/store.tsx
@@ -4,16 +4,15 @@ import { subscribeWithSelector } from "zustand/middleware";
 export default create(
   subscribeWithSelector((set) => {
     return {
-      /**
-       * Flag
-       */
-      isOpenPopup: false,
-
-      togglePopup: () => {
-        set((state: any) => {
-          return { isOpenPopup: !state.isOpenPopup };
-        });
-      },
+      //   /**
+      //    * Flag
+      //    */
+      //   isOpenPopup: false,
+      //   togglePopup: () => {
+      //     set((state: any) => {
+      //       return { isOpenPopup: !state.isOpenPopup };
+      //     });
+      //   },
     };
   }),
 );
