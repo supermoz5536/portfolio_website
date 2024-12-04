af7cac4594c398761519b75645462b3f825d9c21
STF 5 <supermappin@gmail.com>
2024-12-04 21:44:52 +0900

    GunttチャートのUI部分の実装完了

--- app/routes/panel_2/component/graph_gantt/Graph_gantt.tsx
+++ app/routes/panel_2/component/graph_gantt/Graph_gantt.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useRef, useState } from "react";
+import { Tooltip } from "@mui/material";
 import "./graph_gantt.css";
+import { tv } from "tailwind-variants";
+
 type InputData = {
+  projectId: number;
   projectStartYear: number;
   projectStartMonth: number;
   projectStartMonthPeriod: number;
@@ -11,7 +15,47 @@ type InputData = {
   projectTitle: string;
   projectSubtitle: string;
   projectDescription: string;
+  techStacks: string[];
 };
+
+// Tailwind Variants を使ったスタイル設定
+const borderColorStyles = tv({
+  base: "relative top-[-1.25px] left-[-1px] h-[100.5%] w-[100.25%] shadow-md hover:bg-white hover:opacity-75 transition-all duration-300 border border-s-[6px]",
+  variants: {
+    number: {
+      1: "border-[#9FDB85]",
+      2: "border-[#9BB6FF]",
+      3: "border-[#FFCE9B]",
+      4: "border-[#CE9BFF]",
+      5: "border-[#FFBBA8]",
+      6: "border-[#FFA8D9]",
+      7: "border-[#9BCEEE]",
+      8: "border-[#E0CB99]",
+    },
+  },
+  defaultVariants: {
+    number: 1,
+  },
+});
+
+const bgColorStyles = tv({
+  base: "absolute h-full w-full opacity-30",
+  variants: {
+    number: {
+      1: "bg-[#9FDB85]",
+      2: "bg-[#9BB6FF]",
+      3: "bg-[#FFCE9B]",
+      4: "bg-[#CE9BFF]",
+      5: "bg-[#FFBBA8]",
+      6: "bg-[#FFA8D9]",
+      7: "bg-[#9BCEEE]",
+      8: "bg-[#E0CB99]",
+    },
+  },
+  defaultVariants: {
+    number: 1,
+  },
+});
 
 export default function GraphGantt() {
   const scrollRef = useRef<HTMLDivElement | null>(null);
@@ -38,10 +82,11 @@ export default function GraphGantt() {
   const projectTitle = "ChatBus";
   const projectSubtitle = "Random Chat App";
   const projectDescription =
-    "強力なプロジェクトの説明は、利害関係者にロードマップを提供し、詳細に行き詰まることなくビジョンを伝えます。私たちは、あなたが始めるのを助けるために専門家のヒントとサンプル プロジェクトの説明をまとめました。";
+    "強力なプロジェクトの説明はとなくビジョ助けるために専門家のヒントなプロジェクトの説明はとなくビジョ助けるために専門家のヒントなプロジェクトの説明はとなくビジョ助けるために専門家のヒントとサンプル プロジェクトの説明をまとめました。";
 
-  let inputDatas = [
+  let inputDatas: InputData[] = [
     {
+      projectId: 1,
       projectStartYear: projectStartYear,
       projectStartMonth: projectStartMonth,
       projectStartMonthPeriod: projectStartMonthPeriod,
@@ -52,40 +97,50 @@ export default function GraphGantt() {
       projectTitle: projectTitle,
       projectSubtitle: projectSubtitle,
       projectDescription: projectDescription,
+      techStacks: ["Firebase", "Flutter", "React", "Remix", "Three.js"],
     },
     {
+      projectId: 2,
       projectStartYear: projectStartYear,
-      projectStartMonth: projectStartMonth,
+      projectStartMonth: projectStartMonth + 2,
       projectStartMonthPeriod: projectStartMonthPeriod,
       projectEndYear: projectEndYear,
-      projectEndMonth: projectEndMonth,
+      projectEndMonth: projectEndMonth + 2,
       projectEndMonthPeriod: projectEndMonthPeriod,
-      targetStartRow: targetStartRow + 2,
+      targetStartRow: targetStartRow + 1,
       projectTitle: projectTitle,
       projectSubtitle: projectSubtitle,
       projectDescription: projectDescription,
+      techStacks: ["Firebase", "Flutter", "React", "Remix", "Three.js"],
     },
   ];
 
   /// 初期化処理
   useEffect(() => {
-    // スクロール位置の初期配置を指定
-    if (scrollRef.current) {
-      scrollRef.current.scrollLeft =
-        scrollRef.current.scrollWidth - scrollRef.current.clientWidth;
-    }
+    // // スクロール位置の初期配置を指定
+    // if (scrollRef.current) {
+    //   scrollRef.current.scrollLeft =
+    //     scrollRef.current.scrollWidth - scrollRef.current.clientWidth;
+    // }
 
-    /// totalCellsNumbers から占有領域分のセル数を除算
-    let colExtraCellsNumber: any;
+    // totalCellsNumbers から占有領域分のセル数を除算
+    const colExtraCellsNumberAccum: number = inputDatas.reduce(
+      (accum, currentValue, index, array) => {
+        const colStartCell = convertFromInputValueToMapValue(
+          array[index],
+        ).colStartCell;
 
-    inputDatas.map((inputData, index) => {
-      const colStartCell =
-        convertFromInputValueToMapValue(inputData).colStartCell;
-      const colEndCell = convertFromInputValueToMapValue(inputData).colEndCell;
-      colExtraCellsNumber = colEndCell - colStartCell;
-    });
+        const colEndCell = convertFromInputValueToMapValue(
+          array[index],
+        ).colEndCell;
 
-    setTotalCellsNumber((prev) => prev - colExtraCellsNumber);
+        const colExtraCellsNumber = colEndCell - colStartCell;
+        return accum + colExtraCellsNumber;
+      },
+      0,
+    );
+
+    setTotalCellsNumber(totalCellsNumber - colExtraCellsNumberAccum);
   }, []);
 
   /// 全てのグリットの要素を埋める関数
@@ -147,6 +202,7 @@ export default function GraphGantt() {
         (inputData.projectEndMonth - 1) * 5 +
         inputData.projectEndMonthPeriod;
     }
+
     return { colStartCell: colStartCell, colEndCell: colEndCell };
   };
 
@@ -191,29 +247,99 @@ export default function GraphGantt() {
         {/* Cells Content */}
         {inputDatas.map((inputData) => {
           return (
-            // セルの親コンポーネント
-            <div
-              className="relative top-[-1px] left-[-1px] h-[100.25%] w-[100.25%] border border-[#9BCEAE] shadow-md hover:bg-white hover:opacity-75 transition-all duration-300"
-              style={{
-                gridColumnStart:
-                  convertFromInputValueToMapValue(inputData).colStartCell,
-                gridColumnEnd:
-                  convertFromInputValueToMapValue(inputData).colEndCell,
-                gridRowStart: inputData.targetStartRow,
-                gridRowEnd: inputData.targetStartRow + 1,
+            <Tooltip
+              title={
+                <>
+                  <div className="font-bold">Overview :</div>
+                  <div>{inputData.projectDescription}</div>
+                  <br />
+                  <div className="font-bold">Tech Stack :</div>
+                  {inputData.techStacks.map((teckStack, index) => (
+                    <span>
+                      {teckStack}
+                      {index == inputData.techStacks.length - 1 ? "" : ", "}
+                    </span>
+                  ))}
+                </>
+              }
+              followCursor
+              placement="bottom-start"
+              slotProps={{
+                popper: {
+                  modifiers: [
+                    {
+                      name: "offset",
+                      options: {
+                        offset: [10, 15], // ピクセル単位で位置を調整
+                      },
+                    },
+                  ],
+                },
+                tooltip: {
+                  style: {
+                    height: "auto",
+                    width: "225px",
+                    padding: "10px",
+                    color: "black", // テキスト色
+                    backgroundColor: "white", // 背景色
+                    border: "2px solid #cccccc",
+                  },
+                },
               }}
             >
-              {/* 背景色用 */}
-              <div className="absolute h-full w-full  bg-[#82ca9d] opacity-30"></div>
+              <div
+                // セルの親コンポーネント
+                className={`${borderColorStyles({
+                  number:
+                    inputData.projectId % 8 == 0
+                      ? 8
+                      : ((inputData.projectId % 8) as
+                          | 1
+                          | 2
+                          | 3
+                          | 4
+                          | 5
+                          | 6
+                          | 7
+                          | 8),
+                })}`}
+                style={{
+                  gridColumnStart:
+                    convertFromInputValueToMapValue(inputData).colStartCell,
+                  gridColumnEnd:
+                    convertFromInputValueToMapValue(inputData).colEndCell,
+                  gridRowStart: inputData.targetStartRow,
+                  gridRowEnd: inputData.targetStartRow + 1,
+                }}
+              >
+                {/* 背景色用 */}
+                <div
+                  className={`${bgColorStyles({
+                    number:
+                      inputData.projectId % 8 == 0
+                        ? 8
+                        : ((inputData.projectId % 8) as
+                            | 1
+                            | 2
+                            | 3
+                            | 4
+                            | 5
+                            | 6
+                            | 7
+                            | 8),
+                  })}`}
+                ></div>
 
-              {/* テキスト用 */}
-              <div className="absolute top-0 left-1 text-gray-600 font-[425] ml-2 text-lg">
-                {inputData.projectTitle}
+                {/* テキスト用 */}
+                <div className="absolute top-0 left-1 text-gray-600 font-[425] ml-2 text-lg">
+                  {inputData.projectTitle}
+                </div>
+
+                <div className="absolute top-[60%] left-1 text-gray-600 ml-2 mt-[-3px] text-[13.5px]">
+                  {inputData.projectSubtitle}
+                </div>
               </div>
-              <div className="absolute top-[60%] left-1 text-gray-600 ml-2 mt-[-3px] text-[13.5px]">
-                {inputData.projectSubtitle}
-              </div>
-            </div>
+            </Tooltip>
           );
         })}
       </div>
