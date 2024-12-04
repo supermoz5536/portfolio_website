d393e7f774ab5527bf7084617ef1fa9fb21ba1d9
STF 5 <supermappin@gmail.com>
2024-12-04 14:15:18 +0900

    Guntt 修正
    ・現在の日付でのGridの初期化
    ・Gridの縦サイズのレスポンシブル対応

--- app/routes/panel_2/component/graph_gantt/Graph_gantt.tsx
+++ app/routes/panel_2/component/graph_gantt/Graph_gantt.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./graph_gantt.css";
 type InputData = {
   projectStartYear: number;
@@ -14,12 +14,14 @@ type InputData = {
 };
 
 export default function GraphGantt() {
-  // チャート初期設定値
+  const scrollRef = useRef<HTMLDivElement | null>(null);
+
+  // チャート初期値
   const systemStartYear = 2023;
   const startMonth = 11;
   const headerChunkNumber = 5;
-  const rowCellsNumber = 8;
-  const colCellsNumber = 240;
+  const rowCellsNumber = 7;
+  const colCellsNumber = getInitColCellsNumber();
   const headerCellsNumber = colCellsNumber / headerChunkNumber;
   const [totalCellsNumber, setTotalCellsNumber] = useState(
     rowCellsNumber * colCellsNumber,
@@ -31,7 +33,7 @@ export default function GraphGantt() {
   const projectStartMonthPeriod = 1;
   const projectEndYear = 2024;
   const projectEndMonth = 2;
-  const projectEndMonthPeriod = 2;
+  const projectEndMonthPeriod = 1;
   const targetStartRow = 3;
   const projectTitle = "ChatBus";
   const projectSubtitle = "Random Chat App";
@@ -65,18 +67,26 @@ export default function GraphGantt() {
     },
   ];
 
-  /// はみ出る占有領域分のセル数を
-  /// totalCellsNumbers から差し引く初期化処理
+  /// 初期化処理
   useEffect(() => {
-    inputDatas.map((inputData) => {
+    // スクロール位置の初期配置を指定
+    if (scrollRef.current) {
+      scrollRef.current.scrollLeft =
+        scrollRef.current.scrollWidth - scrollRef.current.clientWidth;
+    }
+
+    /// totalCellsNumbers から占有領域分のセル数を除算
+    let colExtraCellsNumber: any;
+
+    inputDatas.map((inputData, index) => {
       const colStartCell =
         convertFromInputValueToMapValue(inputData).colStartCell;
       const colEndCell = convertFromInputValueToMapValue(inputData).colEndCell;
-      const colExtraCellsNumber = colStartCell - colEndCell;
-
-      setTotalCellsNumber((prev) => prev - colExtraCellsNumber);
+      colExtraCellsNumber = colEndCell - colStartCell;
     });
-  }, [inputDatas]);
+
+    setTotalCellsNumber((prev) => prev - colExtraCellsNumber);
+  }, []);
 
   /// 全てのグリットの要素を埋める関数
   /// HeaderとBodyの全てのCellを含む
@@ -140,11 +150,23 @@ export default function GraphGantt() {
     return { colStartCell: colStartCell, colEndCell: colEndCell };
   };
 
+  // 現在の日付からcolCellsNumber（横列の総セル数）を算出
+  function getInitColCellsNumber() {
+    const thisYear = new Date().getFullYear();
+    const thisMonth = new Date().getMonth();
+    // +10： 2023年は11-12月しかない
+    // 年数分のセル：現在年から2023を引いて x 60して加える
+    // 月数分のセル：月は x 5して加える
+    const colCellsNumber = 10 + (thisYear - 2023) * 60 + thisMonth * 5;
+    return colCellsNumber;
+  }
+
   return (
     <>
       {/* 全体枠 */}
       <div
-        className="h-full w-auto bg-white border border-[#C5C5C5] overflow-x-auto overflow-y-auto"
+        ref={scrollRef}
+        className="h-auto w-auto bg-white border border-[#C5C5C5] overflow-x-auto overflow-y-auto"
         style={{
           display: "grid",
           gridTemplateRows: `25px repeat(${rowCellsNumber - 1}, 45px)`,
@@ -169,6 +191,7 @@ export default function GraphGantt() {
         {/* Cells Content */}
         {inputDatas.map((inputData) => {
           return (
+            // セルの親コンポーネント
             <div
               className="relative top-[-1px] left-[-1px] h-[100.25%] w-[100.25%] border border-[#9BCEAE] shadow-md hover:bg-white hover:opacity-75 transition-all duration-300"
               style={{
@@ -180,8 +203,10 @@ export default function GraphGantt() {
                 gridRowEnd: inputData.targetStartRow + 1,
               }}
             >
+              {/* 背景色用 */}
               <div className="absolute h-full w-full  bg-[#82ca9d] opacity-30"></div>
 
+              {/* テキスト用 */}
               <div className="absolute top-0 left-1 text-gray-600 font-[425] ml-2 text-lg">
                 {inputData.projectTitle}
               </div>
