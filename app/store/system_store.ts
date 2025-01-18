7b8c236451c846723118d88cd095508f24e82a66
STF 5 <supermappin@gmail.com>
2025-01-19 07:40:42 +0900

    Store に isPlayerFocus を追加

--- app/store/system_store.ts
+++ app/store/system_store.ts
@@ -3,20 +3,25 @@ import { subscribeWithSelector } from "zustand/middleware";
 
 type SystemStore = {
   isActivated: boolean;
+  isPlayerFocus: boolean;
   toggleIsActivated: () => void;
 };
 
 export const useSystemStore = create<SystemStore>()(
   subscribeWithSelector((set) => {
     return {
-      /**
-       * Scene 2
-       */
       isActivated: false,
+      isPlayerFocus: true,
 
       toggleIsActivated: () => {
         set((state: any) => {
           return { isActivated: !state.isActivated };
+        });
+      },
+
+      setIsPlayerFocus: (newState: boolean) => {
+        set((state: any) => {
+          return { isPlayerFocus: newState };
         });
       },
     };
