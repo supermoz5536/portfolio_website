9f16b022b7216cccdaf1804b8d54e5ba1b00b90a
STF 5 <supermappin@gmail.com>
2025-03-17 22:07:37 +0900

    Scene2: Interfaceの入力受付の機能しない不具合対応のバックアップ直前のバックアップコミット2

--- app/routes/scene_3/Components/view/CanvasScene3.tsx
+++ app/routes/scene_3/Components/view/CanvasScene3.tsx
@@ -3,22 +3,13 @@ import Experience from "../../Experience";
 import {
   EffectComposer,
   Bloom,
-  // Outline,
-  // HueSaturation,
-  // ToneMapping,
-  // Vignette,
-  // Glitch,
-  // Noise,
   DepthOfField,
   ToneMapping,
 } from "@react-three/postprocessing";
-import { useEffect, useLayoutEffect, useRef, useState } from "react";
-
-import { ToneMappingMode, BlendFunction, GlitchMode } from "postprocessing";
-import { Resizer, KernelSize } from "postprocessing";
-import * as THREE from "three";
+import { useEffect, useState } from "react";
+import { ToneMappingMode } from "postprocessing";
+import { KernelSize } from "postprocessing";
 import { OutLineCustom } from "./PostProcessing/Outline/Outline";
-import { useSystemStore } from "~/store/scene3/system_store";
 import { useGlobalStore } from "~/store/global/global_store";
 import { NormalCustom } from "./PostProcessing/Normal/Normal";
 
@@ -90,89 +81,3 @@ export function CanvasScene3() {
     </>
   );
 }
-
-// import { Canvas, useFrame, useThree } from "@react-three/fiber";
-// import Experience from "../../Experience";
-// // import { EffectComposer } from "@react-three/postprocessing";
-// import { useEffect, useMemo, useRef, useState } from "react";
-// import {
-//   MaskPass,
-//   ClearMaskPass,
-//   EffectPass,
-//   RenderPass,
-// } from "postprocessing";
-// import * as THREE from "three";
-// import { OutLineCustom } from "./PostProcessing/Outline/Outline";
-// import { useSystemStore } from "~/store/scene3/system_store";
-// import { useGlobalStore } from "~/store/global/global_store";
-// import { OutLineEffect } from "../view/PostProcessing/Outline/OutlineEffect";
-// import { SavePass } from "three/examples/jsm/postprocessing/SavePass.js";
-// import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
-
-// function MergeComposer() {
-//   const { gl, scene, camera, size } = useThree();
-
-//   const renderTarget = new THREE.WebGLRenderTarget(
-//     window.innerWidth * 1.0,
-//     window.innerHeight * 1.0,
-//     // Options
-//     //    generateMipmaps: 遠くのオブジェクトがぼやけるように自動で解像度を落とす
-//     //    パフォーマンス最適化のために無効化
-//     { generateMipmaps: false },
-//   );
-
-//   const [composerA] = useState<any>(() => new EffectComposer(gl, renderTarget));
-
-//   useEffect(() => {
-//     composerA.renderToScreen = false;
-//     composerA.setSize(size.width, size.height); // サイズ設定
-//     composerA.addPass(new RenderPass(scene, camera)); // レンダーパスを追加
-
-//     // カスタムパスを追加
-//     const outlinePass = new EffectPass(camera, OutLineCustom());
-//     outlinePass.renderToScreen = true;
-
-//     composerA.addPass(outlinePass);
-//   }, [composerA, scene, camera, size]);
-
-//   useFrame((delta) => {
-//     console.log("fadfsafds");
-//     composerA.render(delta);
-//   }, 1);
-
-//   return <primitive object={composerA} />;
-// }
-
-// export function CanvasOutline() {
-//   const isMobile = useGlobalStore((state) => state.isMobile);
-
-//   return (
-//     <>
-//       <Canvas
-//         frameloop="never"
-//         style={{
-//           minHeight: "100vh",
-//           height: "100%",
-//           width: "100%",
-//           zIndex: 0,
-//         }}
-//         shadows
-//         gl={{
-//           localClippingEnabled: true,
-//           alpha: true,
-//           autoClear: false,
-//         }}
-//         camera={{
-//           fov: 45,
-//           near: 0.1,
-//           far: 4000,
-//           position: [0, 0, 100],
-//         }}
-//         dpr={isMobile ? 0.5 : 1.7}
-//       >
-//         <Experience flag="outline" />
-//         <MergeComposer />
-//       </Canvas>
-//     </>
-//   );
-// }
