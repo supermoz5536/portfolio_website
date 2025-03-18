75bb4749a26284ea3e7428fc7cdd84408b49d7a0
STF 5 <supermappin@gmail.com>
2025-03-19 03:40:02 +0900

    Scene3 - 無効化（Player, PlayerShadow）

--- app/routes/scene_3/Components/view/Player.tsx
+++ app/routes/scene_3/Components/view/Player.tsx
@@ -1,27 +1,27 @@
-import { RigidBody } from "@react-three/rapier";
+// import { RigidBody } from "@react-three/rapier";
 
-export function Player() {
-  return (
-    <>
-      <RigidBody
-        position={[0, 10, 7]}
-        canSleep={false}
-        colliders="ball"
-        linearDamping={1.5}
-        angularDamping={2}
-        restitution={0.5}
-        friction={1}
-      >
-        <mesh castShadow receiveShadow>
-          <icosahedronGeometry args={[1, 1]} />
-          <meshStandardMaterial
-            flatShading
-            color={"mediumpurple"}
-            transparent={true}
-            opacity={1}
-          />
-        </mesh>
-      </RigidBody>
-    </>
-  );
-}
+// export function Player() {
+//   return (
+//     <>
+//       <RigidBody
+//         position={[0, 10, 7]}
+//         canSleep={false}
+//         colliders="ball"
+//         linearDamping={1.5}
+//         angularDamping={2}
+//         restitution={0.5}
+//         friction={1}
+//       >
+//         <mesh castShadow receiveShadow>
+//           <icosahedronGeometry args={[1, 1]} />
+//           <meshStandardMaterial
+//             flatShading
+//             color={"mediumpurple"}
+//             transparent={true}
+//             opacity={1}
+//           />
+//         </mesh>
+//       </RigidBody>
+//     </>
+//   );
+// }
