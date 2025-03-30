4f567be0234c4968d1c079389f7d0cbf9eeb025c
STF 5 <supermappin@gmail.com>
2025-03-31 02:49:47 +0900

    Scene1 - Tower 拡大時入りのフレーム落ちを解消
    ・問題 =>
    ・原因 => 画面のマウント中に実行していた
    ・対応 => gasp に delay を設定

--- app/routes/scene_1/Components/view/Tower.tsx
+++ app/routes/scene_1/Components/view/Tower.tsx
@@ -400,7 +400,7 @@ export function Tower() {
         duration: 8,
         scale: 1,
         ease: "power3.inOut",
-        delay: 0,
+        delay: 1,
         onUpdate: () => {
           setAnimationRatio({
             scale: animationRatioRef.current.scale,
