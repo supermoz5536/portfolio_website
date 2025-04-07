6e9aa263b81e63eab00a790fa8ac2d191e5ff34c
STF 5 <supermappin@gmail.com>
2025-04-07 23:40:42 +0900

    Scene3 - OutlineEffectの境界線のDampの実装完了

--- app/routes/scene_3/Components/view/PostProcessing/Outline/OutlineCustom.tsx
+++ app/routes/scene_3/Components/view/PostProcessing/Outline/OutlineCustom.tsx
@@ -1,9 +1,10 @@
 import { useSystemStore } from "~/store/scene3/system_store";
 import { OutLineEffect } from "./OutlineEffect";
 import { useEffect, useRef } from "react";
-import { useThree } from "@react-three/fiber";
+import { useFrame, useThree } from "@react-three/fiber";
 import { useGlobalStore } from "~/store/global/global_store";
 import { Vector2 } from "three";
+import * as THREE from "three";
 
 export function OutLineCustom() {
   const { size } = useThree();
@@ -11,6 +12,8 @@ export function OutLineCustom() {
   const scrollProgress = useSystemStore((state) => state.scrollProgressTopAndTop); // prettier-ignore
   const sizeX = size.width * (isMobile ? 1.0 : 0.75);
   const sizeY = size.height * (isMobile ? 1.0 : 0.75);
+
+  const smoothScrollRatioRef = useRef(0);
 
   const tempVec2Ref = useRef(new Vector2());
   tempVec2Ref.current.x = 1 / sizeX;
@@ -30,10 +33,19 @@ export function OutLineCustom() {
 
     OutlineEffectRef.current.uniforms.get("texelSize")!.value =
       tempVec2Ref.current;
+  }, [sizeX, sizeY]);
+
+  useFrame((state, delta) => {
+    smoothScrollRatioRef.current = THREE.MathUtils.damp(
+      smoothScrollRatioRef.current,
+      scrollProgress,
+      1.25,
+      delta,
+    );
 
     OutlineEffectRef.current.uniforms.get("scrollProgress")!.value =
-      scrollProgress;
-  }, [sizeX, sizeY, scrollProgress]);
+      smoothScrollRatioRef.current;
+  });
 
   return (
     <>
