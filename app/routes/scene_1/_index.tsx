44d76fa159826e8fbd4843857a99d474b956ee39
STF 5 <supermappin@gmail.com>
2025-04-07 10:51:03 +0900

    Scene1 - カメラターゲットの微調整 / CSSのアニメーションの初回限定実行機能の実装

--- app/routes/scene_1/_index.tsx
+++ app/routes/scene_1/_index.tsx
@@ -21,6 +21,7 @@ import { AnimateIn } from "~/components/animate_in";
 
 const visibleDebug = false;
 let isFirstScrolled = false;
+let isOnScreen = true;
 
 export default function Scene1() {
   const [isIntroVisibleAll, setIsIntroVisibleAll] = useState(true);
@@ -29,7 +30,6 @@ export default function Scene1() {
   const [isText2, setIsText2] = useState(false);
   const [isText3, setIsText3] = useState(false);
   const [isLoadingLayer, setIsLoadingLayer] = useState(true);
-  const [isOnScreen, setIsOnScreen] = useState(true);
 
   const isMobileRef = useRef<any>();
   const currentWindowWidthRef = useRef<any>();
@@ -96,9 +96,9 @@ export default function Scene1() {
               }
 
               if (progressRate == 1.0) {
-                setIsOnScreen(false);
+                isOnScreen = false;
               } else {
-                setIsOnScreen(true);
+                isOnScreen = true;
               }
 
               if (progressRate > 0.1) isFirstScrolled = true;
@@ -199,7 +199,10 @@ export default function Scene1() {
   return (
     <div
       id="scene1"
-      className="relative flex justify-center items-center h-[400vh] w-full bg-white"
+      className={
+        "relative flex justify-center items-center h-[400vh] w-full bg-white " +
+        (isOnScreen ? "" : "off-screen")
+      }
     >
       {/* --------
           Scene1
@@ -233,7 +236,7 @@ export default function Scene1() {
               </div>
 
               {/* Block 1 */}
-              <div className="flex flex-col justify-top items-center h-full w-full px-5 pt-20 bg-yellow-0">
+              <div className="flex flex-col justify-top items-center h-full w-full px-5 pt-28 bg-yellow-0">
                 {/* Header */}
                 <h1 className="text-black text-6xl space-x-4">
                   <span className="show-up-1">Paradoxical</span>
@@ -251,7 +254,7 @@ export default function Scene1() {
               {/* Block 2 */}
               <div className="flex flex-col justify-top items-center h-full w-full pt-36 pr-3 space-y-4 show-up-2 bg-green-0">
                 <div className="flex flex-col justify-center items-center ml-28">
-                  <h3 className="text-black text-3xl flowing-underline">
+                  <h3 className="text-black text-3xl flowing-underline exclude-off-screen">
                     What's the factor of mature mind?
                   </h3>
                   <div className="text-black text-base font-light mt-6 space-y-2">
@@ -280,14 +283,14 @@ export default function Scene1() {
               {/* Scroll Icon */}
               <div className="h-10 w-14 ml-8 mr-10">
                 {/* Border Animation */}
-                <div className="stroke is-animated h-full w-full">
+                <div className="stroke is-animated exclude-off-screen h-full w-full">
                   <div className="border top"></div>
                   <div className="border right"></div>
                   <div className="border bottom"></div>
                   <div className="border left"></div>
 
                   {/* Aroow Animation */}
-                  <div className="arrow">
+                  <div className="arrow exclude-off-screen">
                     <span className="invisible">sHolder</span>
                   </div>
                 </div>
@@ -331,7 +334,7 @@ export default function Scene1() {
             {/* Block 2 */}
             <div className="flex flex-col justify-top items-start h-auto w-full pt-12 pr-3 space-y-4 show-up-2 bg-green-0">
               <div className="flex flex-col justify-center items-start px-5">
-                <h3 className="text-black text-[1.6rem] whitespace-nowrap flowing-underline">
+                <h3 className="text-black text-[1.6rem] whitespace-nowrap flowing-underline exclude-off-screen">
                   The factor of mature mind?
                 </h3>
                 <div className="text-black text-base font-light mt-4 ml-4 space-y-2 whitespace-nowrap">
@@ -355,14 +358,14 @@ export default function Scene1() {
               {/* Scroll Icon */}
               <div className="h-10 w-14 ml-4">
                 {/* Border Animation */}
-                <div className="stroke is-animated h-full w-full">
+                <div className="stroke is-animated exclude-off-screen h-full w-full">
                   <div className="border top"></div>
                   <div className="border right"></div>
                   <div className="border bottom"></div>
                   <div className="border left"></div>
 
                   {/* Aroow Animation */}
-                  <div className="arrow">
+                  <div className="arrow exclude-off-screen">
                     <span className="invisible">sHolder</span>
                   </div>
                 </div>
